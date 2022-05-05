f064f04040c8c16734ba5dd8565c91ac41a82597
Baby Shalini <chowlababy@gmailcom>
2022-05-05 23:20:23 +0530

    add cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing-module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CartComponent } from './components/cart/cart.component';
-import { ProductsComponent } from './components/products/products.component';
-
+import { routingComponents } from './app-routing-module';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CartComponent,
-    ProductsComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
